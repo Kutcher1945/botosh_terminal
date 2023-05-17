39c45d4418d37220eaef2caae9fe41339774e38d
Adilan <adilan.akhramovich@gmail.com>
2023-05-17 14:01:53 -0400

    added USBConnector

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import MainPage from './MainPage';
 import PaymentMethodPage from './PaymentMethodPage';
 import ZoomDisable from './ZoomDisable';
 import TouchZoomBlocker from './TouchZoomBlocker'; // Import the TouchZoomBlocker component
+import USBComponent from './USBComponent'; // Import the USBComponent
 
 import './App.css';
 
@@ -14,6 +15,7 @@ export default function App() {
       <div className="App">
         <TouchZoomBlocker /> {/* Include the TouchZoomBlocker component */}
         <ZoomDisable />
+        <USBComponent /> {/* Include the USBComponent */}
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/payment-method" element={<PaymentMethodPage />} />
