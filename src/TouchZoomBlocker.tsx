80bea883e06bb8fe09bbf9e598741504f573c9ab
Adilan <adilan.akhramovich@gmail.com>
2023-05-17 12:50:48 -0400

    touchzoomblocker

--- src/TouchZoomBlocker.tsx
+++ src/TouchZoomBlocker.tsx
@@ -3,19 +3,29 @@ import React, { useEffect } from 'react';
 const TouchZoomBlocker: React.FC = () => {
   useEffect(() => {
     const handleTouchStart = (event: TouchEvent) => {
-      event.preventDefault();
+      if (event.touches.length > 1) {
+        event.preventDefault();
+      }
     };
 
     const handleTouchMove = (event: TouchEvent) => {
+      if (event.touches.length > 1) {
+        event.preventDefault();
+      }
+    };
+
+    const handleContextMenu = (event: TouchEvent | MouseEvent) => {
       event.preventDefault();
     };
 
     document.addEventListener('touchstart', handleTouchStart, { passive: false });
     document.addEventListener('touchmove', handleTouchMove, { passive: false });
+    document.addEventListener('contextmenu', handleContextMenu, { passive: false });
 
     return () => {
       document.removeEventListener('touchstart', handleTouchStart);
       document.removeEventListener('touchmove', handleTouchMove);
+      document.removeEventListener('contextmenu', handleContextMenu);
     };
   }, []);
 
