5f3594e03184cdeb32674bfa8746fb3427832816
Adilan <adilan.akhramovich@gmail.com>
2023-05-17 11:46:13 -0400

    added a helmet to remove touch zoom on tablets

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import PaymentMethodPage from './PaymentMethodPage';
 import ZoomDisable from './ZoomDisable';
+import { Helmet } from 'react-helmet';
 import './App.css';
-
 
 export default function App() {
   return (
     <Router>
       <div className="App">
+        <Helmet>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        </Helmet>
         <ZoomDisable />
         <Routes>
           <Route path="/" element={<MainPage />} />
